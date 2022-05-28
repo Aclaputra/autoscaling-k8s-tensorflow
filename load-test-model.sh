58d57062077a3fa2642d2f5c0acb48c04f0d22fe
Aclaputra <muhamadacla22@gmail.com>
2022-05-29 06:48:33 +0700

    fix: locust location folder

--- load-test-model.sh
+++ load-test-model.sh
@@ -1,6 +1,6 @@
 echo "Load test the model"
 cd locust
-locust -f tasks.py --headless --users 32 --spawn-rate 1 --step-load --step-users 1 --step-time 30s --host http://[EXTERNAL_IP]:8501
+locust -f locust/tasks.py --headless --users 32 --spawn-rate 1 --step-load --step-users 1 --step-time 30s --host http://[EXTERNAL_IP]:8501
 
 echo "-Observe the TF Serving Deployment in GKE dashboard.-"
 echo "-Observe the default node-pool-"
